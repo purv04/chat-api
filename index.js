e2691e2c4fa733f23a05bd0fa1d99cd1561996ec
purv04 <puru.chaturvedi04@gmail.com>
2025-05-22 20:19:44 +0530

    added prometheus

--- index.js
+++ index.js
@@ -37,3 +37,13 @@ const serverInstance = server.listen(PORT, () => {
 });
 
 module.exports = { app, server: serverInstance };
+
+
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
+});
